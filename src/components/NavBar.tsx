590095d33f0df41034ad94ae670b0bd53ed2b598
Xuân Bảo <work.xuanbao@gmail.com>
2025-10-30 18:09:15 +0700

    Refactor NavBar component for client usage
    Removed outdated code and added imports for client usage.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,3 @@
-<<<<<<< Updated upstream
-const NavBar = () => {
-    return (
-        <nav className="sticky top-0 flex items-center justify-center p-4 bg-gray-800 text-white">
-            <div className="flex gap-8 space-x-4">
-                <a href="#" className="hover:text-amber-200 transition-all duration-500">Home</a>
-                <a href="#" className="hover:text-amber-200 transition-all duration-500">About</a>
-                <a href="#" className="hover:text-amber-200 transition-all duration-500">Services</a>
-                <a href="#" className="hover:text-amber-200 transition-all duration-500">Contact</a>
-            </div>
-        </nav>
-    );
-=======
 "use client";
 import { logout } from "@/services/authService";
 import Image from "next/image";
